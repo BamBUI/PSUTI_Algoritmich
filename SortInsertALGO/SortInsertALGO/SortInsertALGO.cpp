71579424e765f3aea6c3d542055f6313d57b87b6
BamBUI <72223826+BamBUI@users.noreply.github.com>
2020-12-18 19:50:28 +0400

    Сортировка вставками

---
+++ SortInsertALGO/SortInsertALGO/SortInsertALGO.cpp
@@ -0,0 +1,43 @@
+﻿// C program for insertion sort 
+#include <math.h> 
+#include <stdio.h> 
+
+// Функция для сортировки массива используя сортировку вставки
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        // Переместите элементы arr [0..i-1], которые
+        // больше ключа, на одну позицию впереди
+        // своего текущего положения 
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Функция для печати массива размера n
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+//Программа для проверки сортировки вставками
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
